5cf861d0abb8e5102672df6619bd0101dd35f929
Phil-Mobile <formaphil@gmail.com>
2026-06-07 11:25:34 +0200

    Update sw.js
    mise à jour du cache après design

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@
                 Network First pour les pages HTML
    ============================================================ */
 
-const CACHE_NAME = 'lacasadudon-v9';
-const CACHE_STATIC = 'lacasadudon-static-v9';
-const CACHE_IMAGES = 'lacasadudon-images-v9';
+const CACHE_NAME = 'lacasadudon-v10';
+const CACHE_STATIC = 'lacasadudon-static-v10';
+const CACHE_IMAGES = 'lacasadudon-images-v10';
 
 /* ── Assets à mettre en cache immédiatement (App Shell) ── */
 const PRECACHE_URLS = [
