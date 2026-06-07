745b042b133317223def1579a47f80b79ded5133
Phil-Mobile <formaphil@gmail.com>
2026-06-07 11:19:10 +0200

    Update sw.js
    update caceh

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@
                 Network First pour les pages HTML
    ============================================================ */
 
-const CACHE_NAME = 'lacasadudon-v8';
-const CACHE_STATIC = 'lacasadudon-static-v8';
-const CACHE_IMAGES = 'lacasadudon-images-v8';
+const CACHE_NAME = 'lacasadudon-v9';
+const CACHE_STATIC = 'lacasadudon-static-v9';
+const CACHE_IMAGES = 'lacasadudon-images-v9';
 
 /* ── Assets à mettre en cache immédiatement (App Shell) ── */
 const PRECACHE_URLS = [
